a80dc8b26782d5ab7799ecbae450647edd8cd046
carkings <qingzi.wang@cq-tct.com>
2017-05-28 14:00:41 +0800

    fixed babel es6

---
+++ vue/vue-seed/src/components/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>Hello carkings</h1>
+    <center>{{ msg }}</center>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
