5a03df6ffb39427f180831d89a04bf2e58773958
carkings <qingzi.wang@cq-tct.com>
2017-08-10 10:10:18 +0800

    show webpack config and add less

--- react/react-seed2/src/components/App.jsx
+++ react/react-seed2/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link} from 'react-router-dom';
-import './App.css';
+import './App.less';
 
 class App extends Component {
   render() {
--- react/react-seed2/src/index.js
+++ react/react-seed2/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.less';
 
 import Routes from './routes/index';
 import registerServiceWorker from './registerServiceWorker';
